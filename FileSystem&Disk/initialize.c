c4b374104bfd59f5a44f8d47938e3937cb9b086e
kysu5095 <kysu5095@gmail.com>
2020-05-27 02:32:34 +0900

    version_1.5 modify initialize.c

--- FileSystem&Disk/initialize.c
+++ FileSystem&Disk/initialize.c
@@ -10,42 +10,46 @@
 //#include <io.h>
 #include <unistd.h>
 
+FileSysInfo* pFileSysInfo;
+void FileSysInit(void);
+
 /* File System Initialize */ 
 void FileSysInit(void){
-    char* file_sys_info = (char*)malloc(BLOCK_SIZE);
-    char* inode_bytemap = (char*)malloc(BLOCK_SIZE);
-    char* block_bytemap = (char*)malloc(BLOCK_SIZE);
-    char* inodelist_1   = (char*)malloc(BLOCK_SIZE);
-    char* inodelist_2   = (char*)malloc(BLOCK_SIZE);
-    char* inodelist_3   = (char*)malloc(BLOCK_SIZE);
-    char* inodelist_4   = (char*)malloc(BLOCK_SIZE);
+    pFileSysInfo = (FileSysInfo*)malloc(sizeof(FileSysInfo));
+    pFileSysInfo->blocks            = BLOCK_SIZE;
+    pFileSysInfo->rootInodeNum      = 0;
+    pFileSysInfo->diskCapacity      = FS_DISK_CAPACITY;
+    pFileSysInfo->numAllocBlocks    = 7;
+    pFileSysInfo->numFreeBlocks     = BLOCK_SIZE - 7;    
+    pFileSysInfo->numAllocInodes    = 1;
+    pFileSysInfo->blockBytemapBlock = BLOCK_BYTEMAP_BLOCK_NUM;
+    pFileSysInfo->inodeBytemapBlock = INODE_BYTEMAP_BLOCK_NUM;
+    pFileSysInfo->inodeListBlock    = INODELIST_BLOCK_FIRST;
+    pFileSysInfo->dataRegionBlock   = 7;
 
-    memset(file_sys_info, 0, sizeof(file_sys_info));
-    memset(inode_bytemap, 0, sizeof(inode_bytemap));
-    memset(block_bytemap, 0, sizeof(block_bytemap));
-    memset(inodelist_1,   0, sizeof(inodelist_1));
-    memset(inodelist_2,   0, sizeof(inodelist_2));
-    memset(inodelist_3,   0, sizeof(inodelist_3));
-    memset(inodelist_4,   0, sizeof(inodelist_4));
+    char* file_sys_info_block = (char*)malloc(BLOCK_SIZE);
+    char* inode_bytemap_block = (char*)malloc(BLOCK_SIZE);
+    char* block_bytemap_block = (char*)malloc(BLOCK_SIZE);
+    char* inodelist_1_block   = (char*)malloc(BLOCK_SIZE);
+    char* inodelist_2_block   = (char*)malloc(BLOCK_SIZE);
+    char* inodelist_3_block   = (char*)malloc(BLOCK_SIZE);
+    char* inodelist_4_block   = (char*)malloc(BLOCK_SIZE);
 
-    DevWriteBlock(0, file_sys_info);
-    DevWriteBlock(1, inode_bytemap);
-    DevWriteBlock(2, block_bytemap);
-    DevWriteBlock(3, inodelist_1);
-    DevWriteBlock(4, inodelist_2);
-    DevWriteBlock(5, inodelist_3);
-    DevWriteBlock(6, inodelist_4);
+    memset(file_sys_info_block, 0, sizeof(file_sys_info_block));
+    memset(inode_bytemap_block, 0, sizeof(inode_bytemap_block));
+    memset(block_bytemap_block, 0, sizeof(block_bytemap_block));
+    memset(inodelist_1_block,   0, sizeof(inodelist_1_block));
+    memset(inodelist_2_block,   0, sizeof(inodelist_2_block));
+    memset(inodelist_3_block,   0, sizeof(inodelist_3_block));
+    memset(inodelist_4_block,   0, sizeof(inodelist_4_block));
 
-    // FileSysInfo* file_sys_info = (FileSysInfo*)malloc(sizeof(FileSysInfo));
-    // file_sys_info->blocks = BLOCK_SIZE;
-    // file_sys_info->rootInodeNum = 0;
-    // file_sys_info->diskCapacity = BLOCK_SIZE * BLOCK_SIZE;
-    // file_sys_info->numAllocBlocks = 0;
-    // file_sys_info->numFreeBlocks = BLOCK_SIZE;
-    // file_sys_info->numAllocInodes = 0;
-    // file_sys_info->blockBytemapBlock = 2;
-    // file_sys_info->inodeBytemapBlock = 1;
-    // file_sys_info->inodeListBlock = 3;
-    // file_sys_info->dataRegionBlock = 7;
-    // DevWriteBlock(0, (char*)file_sys_info);
+    memcpy(file_sys_info_block, pFileSysInfo, BLOCK_SIZE);
+
+    DevWriteBlock(0, file_sys_info_block);
+    DevWriteBlock(1, inode_bytemap_block);
+    DevWriteBlock(2, block_bytemap_block);
+    DevWriteBlock(3, inodelist_1_block);
+    DevWriteBlock(4, inodelist_2_block);
+    DevWriteBlock(5, inodelist_3_block);
+    DevWriteBlock(6, inodelist_4_block);
 }
