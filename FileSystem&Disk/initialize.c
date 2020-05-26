8bb4d8a360db9f19048ccbe1069eca75d5244d08
kysu5095 <kysu5095@gmail.com>
2020-05-26 15:42:03 +0900

    version_1.3 modify initialize.c

--- FileSystem&Disk/initialize.c
+++ FileSystem&Disk/initialize.c
@@ -13,5 +13,15 @@
 /* File System Initialize */ 
 void FileSysInit(void){
     FileSysInfo* file_sys_info = (FileSysInfo*)malloc(sizeof(FileSysInfo));
-
+    file_sys_info->blocks = BLOCK_SIZE;
+    file_sys_info->rootInodeNum = 0;
+    file_sys_info->diskCapacity = BLOCK_SIZE * BLOCK_SIZE;
+    file_sys_info->numAllocBlocks = 0;
+    file_sys_info->numFreeBlocks = BLOCK_SIZE;
+    file_sys_info->numAllocInodes = 0;
+    file_sys_info->blockBytemapBlock = 2;
+    file_sys_info->inodeBytemapBlock = 1;
+    file_sys_info->inodeListBlock = 3;
+    file_sys_info->dataRegionBlock = 7;
+    DevWriteBlock(0, (char*)file_sys_info);
 }
