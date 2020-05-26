dba90c664f3ece960e3d792ec9908f7e23a79549
kysu5095 <kysu5095@gmail.com>
2020-05-27 03:37:50 +0900

    version_1.7 modify fs_internal.c

--- FileSystem&Disk/fs_internal.c
+++ FileSystem&Disk/fs_internal.c
@@ -3,49 +3,73 @@
 #include "disk.h"
 #include "fs.h"
 
-void SetInodeBytemap(int inodeno)
-{
-
+void SetInodeBytemap(int inodeno) {
+    char* inode = (char*)malloc(sizeof(BLOCK_SIZE));
+    DevReadBlock(1, inode);
+    inode[inodeno] = 1;
+    DevWriteBlock(1, inode);
 }
 
 
-void ResetInodeBytemap(int inodeno)
-{
-
+void ResetInodeBytemap(int inodeno) {
+    char* inode = (char*)malloc(sizeof(BLOCK_SIZE));
+    DevReadBlock(1, inode);
+    inode[inodeno] = 0;
+    DevWriteBlock(1, inode);
 }
 
 
-void SetBlockBytemap(int blkno)
-{
-
+void SetBlockBytemap(int blkno) {
+    char* block = (char*)malloc(sizeof(BLOCK_SIZE));
+    DevReadBlock(2, block);
+    block[blkno] = 1;
+    DevWriteBlock(2, block);
 }
 
 
-void ResetBlockBytemap(int blkno)
-{
-
+void ResetBlockBytemap(int blkno) {
+    char* block = (char*)malloc(sizeof(BLOCK_SIZE));
+    DevReadBlock(2, block);
+    block[blkno] = 0;
+    DevWriteBlock(2, block);
 }
 
 
-void PutInode(int inodeno, Inode* pInode)
-{
-
+void PutInode(int inodeno, Inode* pInode) {
+    char* inode_list = (char*)malloc(sizeof(BLOCK_SIZE));
+    int block_idx = (inodeno / NUM_OF_INODE_PER_BLOCK) + 3;
+    DevReadBlock(block_idx, inode_list);
+    int node_idx = inodeno % NUM_OF_INODE_PER_BLOCK;
+    memcpy(inode_list + (node_idx * sizeof(Inode)), pInode, sizeof(Inode));
 }
 
 
-void GetInode(int inodeno, Inode* pInode)
-{
-
+void GetInode(int inodeno, Inode* pInode) {
+    char* inode_list = (char*)malloc(sizeof(BLOCK_SIZE));
+    int block_idx = (inodeno / NUM_OF_INODE_PER_BLOCK) + 3;
+    DevReadBlock(block_idx, inode_list);
+    int node_idx = inodeno % NUM_OF_INODE_PER_BLOCK;
+    memcpy(pInode, inode_list + (node_idx * sizeof(Inode)), sizeof(Inode));
 }
 
 
-int GetFreeInodeNum(void)
-{
-
+int GetFreeInodeNum(void) {
+    char* inode = (char*)malloc(sizeof(BLOCK_SIZE));
+    DevReadBlock(1, inode);
+    for(int i = 0; i < MAX_FD_ENTRY_MAX; i++)
+        if(inode[i] == '0')
+            return i;
+    
+    return -1;
 }
 
 
-int GetFreeBlockNum(void)
-{
-
+int GetFreeBlockNum(void) {
+    char* block = (char*)malloc(sizeof(BLOCK_SIZE));
+    DevReadBlock(2, block);
+    for(int i = 0; i < MAX_FD_ENTRY_MAX; i++)
+        if(block[i] == '0')
+            return i;
+    
+    return -1;
 }
