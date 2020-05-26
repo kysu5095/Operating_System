3737d805b3106b5e1e8dd8b1113e8f35e26404de
kysu5095 <kysu5095@gmail.com>
2020-05-26 13:24:28 +0900

    version_1.1 create initialize.c

---
+++ FileSystem&Disk/initialize.c
@@ -0,0 +1,17 @@
+#include "disk.h"
+#include "fs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <assert.h>
+#include <io.h>
+//#include <unistd.h>
+
+/* File System Initialize */ 
+void FileSysInit(void){
+    FileSysInfo* file_sys_info = (FileSysInfo*)malloc(sizeof(FileSysInfo));
+    
+}
