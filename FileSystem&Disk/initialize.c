54c904863e9e72eaee91d054ea55d40bf90a5428
kysu5095 <kysu5095@gmail.com>
2020-05-27 20:13:41 +0900

    version_2.6 modify initialize.c : FileSysInit()

--- FileSystem&Disk/initialize.c
+++ FileSystem&Disk/initialize.c
@@ -7,7 +7,6 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <assert.h>
-//#include <io.h>
 #include <unistd.h>
 
 FileSysInfo* pFileSysInfo;
@@ -30,30 +29,6 @@ void FileSysInit(void){
     memset(inodelist_2_block,   '0', sizeof(char) * BLOCK_SIZE);
     memset(inodelist_3_block,   '0', sizeof(char) * BLOCK_SIZE);
     memset(inodelist_4_block,   '0', sizeof(char) * BLOCK_SIZE);
-
-    // /* file system information block */
-    // pFileSysInfo = (FileSysInfo*)malloc(sizeof(FileSysInfo));
-    // pFileSysInfo->blocks            = BLOCK_SIZE;
-    // pFileSysInfo->rootInodeNum      = 0;
-    // pFileSysInfo->diskCapacity      = FS_DISK_CAPACITY;
-    // pFileSysInfo->numAllocBlocks    = 7;
-    // pFileSysInfo->numFreeBlocks     = BLOCK_SIZE - 7;    
-    // pFileSysInfo->numAllocInodes    = 1;
-    // pFileSysInfo->blockBytemapBlock = BLOCK_BYTEMAP_BLOCK_NUM;
-    // pFileSysInfo->inodeBytemapBlock = INODE_BYTEMAP_BLOCK_NUM;
-    // pFileSysInfo->inodeListBlock    = INODELIST_BLOCK_FIRST;
-    // pFileSysInfo->dataRegionBlock   = 7;
-    // memcpy(file_sys_info_block, pFileSysInfo, BLOCK_SIZE);
-
-    // /* inode bytemap block */
-    // char inode_bytemap[MAX_FD_ENTRY_MAX];
-    // memset(inode_bytemap, 0, sizeof(inode_bytemap));
-    // inode_bytemap[0] = '1';
-    // memcpy(inode_bytemap_block, inode_bytemap, BLOCK_SIZE);
-
-    // /* block bytemap block */
-    // char block_bytemap[MAX_FD_ENTRY_MAX];
-    // memset(block_bytemap, 0, sizeof(block_bytemap));
 
     DevWriteBlock(0, file_sys_info_block);
     DevWriteBlock(1, inode_bytemap_block);
