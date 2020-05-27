fe2e94668e9a2aac5af30525190eb17f501a2ffe
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-05-28 00:56:38 +0900

    version_2.12 modify initialize.c : FileSysInit()

--- FileSystem&Disk/initialize.c
+++ FileSystem&Disk/initialize.c
@@ -14,29 +14,29 @@ void FileSysInit(void);
 
 /* File System Initialize */ 
 void FileSysInit(void){
-    char* file_sys_info_block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
-    char* inode_bytemap_block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
-    char* block_bytemap_block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
-    char* inodelist_1_block   = (char*)malloc(sizeof(char) * BLOCK_SIZE);
-    char* inodelist_2_block   = (char*)malloc(sizeof(char) * BLOCK_SIZE);
-    char* inodelist_3_block   = (char*)malloc(sizeof(char) * BLOCK_SIZE);
-    char* inodelist_4_block   = (char*)malloc(sizeof(char) * BLOCK_SIZE);
+    FileSysInfo* file_sys_info_block = (FileSysInfo*)malloc(sizeof(BLOCK_SIZE));
+    char* inode_bytemap_block = (char*) malloc(sizeof(char)  * BLOCK_SIZE);
+    char* block_bytemap_block = (char*) malloc(sizeof(char)  * BLOCK_SIZE);
+    Inode* inodelist_1_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+    Inode* inodelist_2_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+    Inode* inodelist_3_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+    Inode* inodelist_4_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
 
-    memset(file_sys_info_block, '0', sizeof(char) * BLOCK_SIZE);
+    memset(file_sys_info_block, 0, sizeof(BLOCK_SIZE));
     memset(inode_bytemap_block, '0', sizeof(char) * BLOCK_SIZE);
     memset(block_bytemap_block, '0', sizeof(char) * BLOCK_SIZE);
-    memset(inodelist_1_block,   '0', sizeof(char) * BLOCK_SIZE);
-    memset(inodelist_2_block,   '0', sizeof(char) * BLOCK_SIZE);
-    memset(inodelist_3_block,   '0', sizeof(char) * BLOCK_SIZE);
-    memset(inodelist_4_block,   '0', sizeof(char) * BLOCK_SIZE);
+    memset(inodelist_1_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+    memset(inodelist_2_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+    memset(inodelist_3_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+    memset(inodelist_4_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
 
-    DevWriteBlock(0, file_sys_info_block);
+    DevWriteBlock(0, (char*)file_sys_info_block);
     DevWriteBlock(1, inode_bytemap_block);
     DevWriteBlock(2, block_bytemap_block);
-    DevWriteBlock(3, inodelist_1_block);
-    DevWriteBlock(4, inodelist_2_block);
-    DevWriteBlock(5, inodelist_3_block);
-    DevWriteBlock(6, inodelist_4_block);
+    DevWriteBlock(3, (char*)inodelist_1_block);
+    DevWriteBlock(4, (char*)inodelist_2_block);
+    DevWriteBlock(5, (char*)inodelist_3_block);
+    DevWriteBlock(6, (char*)inodelist_4_block);
     for(int i = 7; i < BLOCK_SIZE; i++){
         char* block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
         memset(block, '0', sizeof(char) * BLOCK_SIZE);
