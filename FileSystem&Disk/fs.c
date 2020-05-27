9bbdf48a180b3dc5e4711e36b89174785a96c425
kysu5095 <kysu5095@gmail.com>
2020-05-27 19:57:26 +0900

    version_2.4 modify fs.c

--- FileSystem&Disk/fs.c
+++ FileSystem&Disk/fs.c
@@ -1,71 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "disk.h"
 #include "fs.h"
 
-int		CreateFile(const char* szFileName)
-{
+int		CreateFile(const char* szFileName) {
 
 }
 
-int		OpenFile(const char* szFileName)
-{
-
-}
-
-
-int		WriteFile(int fileDesc, char* pBuffer, int length)
-{
-
-}
-
-int		ReadFile(int fileDesc, char* pBuffer, int length)
-{
+int		OpenFile(const char* szFileName) {
 
 }
 
 
-int		CloseFile(int fileDesc)
-{
+int		WriteFile(int fileDesc, char* pBuffer, int length) {
 
 }
 
-int		RemoveFile(const char* szFileName)
-{
-
-}
-
-
-int		MakeDir(const char* szDirName)
-{
+int		ReadFile(int fileDesc, char* pBuffer, int length) {
 
 }
 
 
-int		RemoveDir(const char* szDirName)
-{
+int		CloseFile(int fileDesc) {
 
 }
 
-int   EnumerateDirStatus(const char* szDirName, DirEntryInfo* pDirEntry, int dirEntrys)
-{
-
-}
-
-
-void	CreateFileSystem()
-{
+int		RemoveFile(const char* szFileName) {
 
 }
 
 
-void	OpenFileSystem()
-{
+int		MakeDir(const char* szDirName) {
 
 }
 
 
-void	CloseFileSystem()
-{
+int		RemoveDir(const char* szDirName) {
+
+}
+
+int   EnumerateDirStatus(const char* szDirName, DirEntryInfo* pDirEntry, int dirEntrys) {
+
+}
+
+
+void	CreateFileSystem() {
+    int block_idx = GetFreeBlockNum();
+    int inode_idx = GetFreeInodeNum();
+    DirEntry* dir = (DirEntry*)malloc(sizeof(DirEntry) * NUM_OF_DIRENT_PER_BLOCK);
+    strcpy(dir[0].name, ".");
+    dir[0].inodeNum = 0;
+    DevWriteBlock(7, (char*)dir);
+}
+
+
+void	OpenFileSystem() {
+
+}
+
+
+void	CloseFileSystem() {
 
 }
