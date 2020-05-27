34cab53378c943c76dca1e86861f0839b26ba0c2
kysu5095 <kysu5095@gmail.com>
2020-05-27 20:01:50 +0900

    experiment main

--- FileSystem&Disk/main.c
+++ FileSystem&Disk/main.c
@@ -1,6 +1,7 @@
 #include "fs.h"
 #include "disk.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 int main(){
@@ -10,5 +11,9 @@ int main(){
 	char* block = (char*)malloc(BLOCK_SIZE);
 	DevReadBlock(0, block);
 	printf("%s\n", block);
+	CreateFileSystem();
+    DirEntry* dir = (DirEntry*)malloc(sizeof(DirEntry) * NUM_OF_DIRENT_PER_BLOCK);
+	DevReadBlock(7, (char*)dir);
+	printf("%d : %s(%d)\n", dir[0].inodeNum, dir[0].name, (int)strlen(dir[0].name));
 	return 0;
 }
