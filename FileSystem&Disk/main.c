d3629e5fe2675592938a819c46e477072d4fadfc
kysu5095 <kysu5095@gmail.com>
2020-05-27 13:37:48 +0900

    version_1.10 experiment main

--- FileSystem&Disk/main.c
+++ FileSystem&Disk/main.c
@@ -1,8 +1,14 @@
 #include "fs.h"
 #include "disk.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
 	printf("Hello OS World\n");
+	DevCreateDisk();
+	FileSysInit();
+	char* block = (char*)malloc(BLOCK_SIZE);
+	DevReadBlock(0, block);
+	printf("%s\n", block);
 	return 0;
 }
