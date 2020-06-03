2a13c951a8e597c03ed445cf42dee918a2f6e9ed
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-06-04 03:34:43 +0900

    experiment main

--- FileSystem&Disk/main.c
+++ FileSystem&Disk/main.c
@@ -8,34 +8,67 @@ int main(){
 	printf("Hello OS World\n");
 	CreateFileSystem();
 	
-	// char* block = (char*)malloc(BLOCK_SIZE);
-	// DevReadBlock(0, block);
-	// printf("%s\n", block);
-    // DirEntry* dir = (DirEntry*)malloc(sizeof(DirEntry) * NUM_OF_DIRENT_PER_BLOCK);
-	// DevReadBlock(7, (char*)dir);
-	// printf("%d : %s(%d)\n", dir[0].inodeNum, dir[0].name, (int)strlen(dir[0].name));
-    // FileSysInfo* dir2 = (FileSysInfo*)malloc(sizeof(FileSysInfo) * NUM_OF_DIRENT_PER_BLOCK);
-	// DevReadBlock(0, (char*)dir2);
-	// printf("Blocks : %d\n", dir2->blocks);
-	// printf("RootInodeNum : %d\n", dir2->rootInodeNum);
-	// printf("DiskCapacity : %d\n", dir2->diskCapacity);
-	// printf("NumAllocBlocks : %d\n", dir2->numAllocBlocks);
-	// printf("NumFreeBlocks : %d\n", dir2->numFreeBlocks);
-	// printf("NumAllocInodes : %d\n", dir2->numAllocInodes);
-	// printf("BlockByteMapBlock : %d\n", dir2->blockBytemapBlock);
-	// printf("InodeByteMapBlock : %d\n", dir2->inodeBytemapBlock);
-	// printf("InodeListBlock : %d\n", dir2->inodeListBlock);
-	// printf("DataRegionBlock : %d\n", dir2->dataRegionBlock);
+	// printf("Blocks : %d\n", pFileSysInfo->blocks);
+	// printf("RootInodeNum : %d\n", pFileSysInfo->rootInodeNum);
+	// printf("DiskCapacity : %d\n", pFileSysInfo->diskCapacity);
+	// printf("NumAllocBlocks : %d\n", pFileSysInfo->numAllocBlocks);
+	// printf("NumFreeBlocks : %d\n", pFileSysInfo->numFreeBlocks);
+	// printf("NumAllocInodes : %d\n", pFileSysInfo->numAllocInodes);
+	// printf("BlockByteMapBlock : %d\n", pFileSysInfo->blockBytemapBlock);
+	// printf("InodeByteMapBlock : %d\n", pFileSysInfo->inodeBytemapBlock);
+	// printf("InodeListBlock : %d\n", pFileSysInfo->inodeListBlock);
+	// printf("DataRegionBlock : %d\n", pFileSysInfo->dataRegionBlock);
 
-	printf("directory : %d\n", NUM_OF_DIRENT_PER_BLOCK);
-	printf("inode : %d\n", NUM_OF_INODE_PER_BLOCK);
-	for(int i = 0; i < 50; i++){
+	DirEntry* ddir = (DirEntry*)malloc(sizeof(DirEntry) * NUM_OF_DIRENT_PER_BLOCK);
+	char* b = (char*)malloc(100);
+	for(int i = 0; i < 40; i++){
 		int a = i;
-		char b[5];
 		sprintf(b, "/tmp%d", a);
 		printf("%s\n", b);
 		MakeDir(b);
+		DevReadBlock(7, (char*)ddir);
 	}
+	 free(ddir);
+	// printf("===========remove==========\n");
+	// for(int i = 0; i < 20; i++){
+	// 	int a = i;
+	// 	char b[5];
+	// 	sprintf(b, "/tmp%d", a);
+	// 	printf("%s\n", b);
+	// 	RemoveDir(b);
+	// }
 
+	// Inode* inode = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+	// DevReadBlock(0, (char*)inode);
+	// for(int i = 0; i < NUM_OF_INODE_PER_BLOCK; i++){
+	// 	inode[i].allocBlocks = i + 1;
+	// 	inode[i].size = i + 100;
+	// 	inode[i].type = i + 10000;
+	// 	for(int j = 0; j < NUM_OF_DIRECT_BLOCK_PTR; j++)
+	// 		inode[i].dirBlockPtr[j] = i + j + 1000000;
+	// }
+	// DevWriteBlock(0, (char*)inode);
+	// free(inode);
+	// Inode* inode2 = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
+	// DevReadBlock(0, (char*)inode2);
+	// for(int i = 0; i < NUM_OF_INODE_PER_BLOCK; i++){
+	// 	printf("BLOCK : %d  SIZE : %d  TYPE : %d  ", inode2[i].allocBlocks, inode2[i].size, inode2[i].type);
+	// 	for(int j = 0; j < NUM_OF_DIRECT_BLOCK_PTR; j++){
+	// 		printf("  PTR%d : %d", j, inode2[i].dirBlockPtr[j]);
+	// 	}
+	// 	printf("\n");
+	// }
+	// free(inode2);
+	// char* tmp = (char*)malloc(BLOCK_SIZE);
+	// DevReadBlock(2, tmp);
+	// printf("%s(%d)\n", tmp, (int)strlen(tmp));
+	// for(int i = 0; i < 10; i++)
+	// 	SetBlockBytemap(i);
+	// DevReadBlock(2, tmp);
+	// printf("%s(%d)\n", tmp, (int)strlen(tmp));
+	// free(tmp);
+
+	DevCloseDisk();
+	printf("finish\n");
 	return 0;
 }
