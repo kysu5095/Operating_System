6e6d59bd5a0c34650980d1ea7eb2775a82515de0
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-06-05 01:43:56 +0900

    FileSysInit() : change inode bytemap, block byte map type char to int

--- FileSystem&Disk/initialize.c
+++ FileSystem&Disk/initialize.c
@@ -15,24 +15,24 @@ void FileSysInit(void);
 /* File System Initialize */ 
 void FileSysInit(void){
     pFileSysInfo = (FileSysInfo*)malloc(BLOCK_SIZE);
-    char* inode_bytemap_block = (char*) malloc(sizeof(char)  * BLOCK_SIZE);
-    char* block_bytemap_block = (char*) malloc(sizeof(char)  * BLOCK_SIZE);
+    int* inode_bytemap_block = (int*) malloc(BLOCK_SIZE);
+    int* block_bytemap_block = (int*) malloc(BLOCK_SIZE);
     Inode* inodelist_1_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
     Inode* inodelist_2_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
     Inode* inodelist_3_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
     Inode* inodelist_4_block  = (Inode*)malloc(sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
 
     memset(pFileSysInfo, 0, BLOCK_SIZE);
-    memset(inode_bytemap_block, '0', sizeof(char) * BLOCK_SIZE);
-    memset(block_bytemap_block, '0', sizeof(char) * BLOCK_SIZE);
+    memset(inode_bytemap_block, 0, BLOCK_SIZE);
+    memset(block_bytemap_block, 0, BLOCK_SIZE);
     memset(inodelist_1_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
     memset(inodelist_2_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
     memset(inodelist_3_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
     memset(inodelist_4_block, 0, sizeof(Inode) * NUM_OF_INODE_PER_BLOCK);
 
     DevWriteBlock(0, (char*)pFileSysInfo);
-    DevWriteBlock(1, inode_bytemap_block);
-    DevWriteBlock(2, block_bytemap_block);
+    DevWriteBlock(1, (char*)inode_bytemap_block);
+    DevWriteBlock(2, (char*)block_bytemap_block);
     DevWriteBlock(3, (char*)inodelist_1_block);
     DevWriteBlock(4, (char*)inodelist_2_block);
     DevWriteBlock(5, (char*)inodelist_3_block);
