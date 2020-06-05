6ba5b5c41411dc25f450083fa5d0351d696c1f2c
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-06-05 14:43:36 +0900

    inode, block function() : change character '1' to 1

--- FileSystem&Disk/fs_internal.c
+++ FileSystem&Disk/fs_internal.c
@@ -7,7 +7,7 @@
 void SetInodeBytemap(int inodeno) {
     char* inode = (char*)malloc(sizeof(char) * BLOCK_SIZE);
     DevReadBlock(INODE_BYTEMAP_BLOCK_NUM, inode);
-    inode[inodeno] = '1';
+    inode[inodeno] = 1;
     DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM, inode);
     free(inode);
 }
@@ -16,7 +16,7 @@ void SetInodeBytemap(int inodeno) {
 void ResetInodeBytemap(int inodeno) {
     char* inode = (char*)malloc(sizeof(char) * BLOCK_SIZE);
     DevReadBlock(INODE_BYTEMAP_BLOCK_NUM, inode);
-    inode[inodeno] = '0';
+    inode[inodeno] = 0;
     DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM, inode);
     free(inode);
 }
@@ -25,7 +25,7 @@ void ResetInodeBytemap(int inodeno) {
 void SetBlockBytemap(int blkno) {
     char* block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
     DevReadBlock(BLOCK_BYTEMAP_BLOCK_NUM, block);
-    block[blkno] = '1';
+    block[blkno] = 1;
     DevWriteBlock(BLOCK_BYTEMAP_BLOCK_NUM, block);
     free(block);
 }
@@ -34,7 +34,7 @@ void SetBlockBytemap(int blkno) {
 void ResetBlockBytemap(int blkno) {
     char* block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
     DevReadBlock(BLOCK_BYTEMAP_BLOCK_NUM, block);
-    block[blkno] = '0';
+    block[blkno] = 0;
     DevWriteBlock(BLOCK_BYTEMAP_BLOCK_NUM, block);
     free(block);
 }
@@ -65,7 +65,7 @@ int GetFreeInodeNum(void) {
     char* inode = (char*)malloc(sizeof(char) * BLOCK_SIZE);
     DevReadBlock(1, inode);
     for(int i = 0; i < MAX_FD_ENTRY_MAX; i++)
-        if(inode[i] == '0')
+        if(inode[i] == 0)
             return i;
     free(inode);
     return -1;
@@ -76,7 +76,7 @@ int GetFreeBlockNum(void) {
     char* block = (char*)malloc(sizeof(char) * BLOCK_SIZE);
     DevReadBlock(2, block);
     for(int i = 7; i < BLOCK_SIZE; i++)
-        if(block[i] == '0')
+        if(block[i] == 0)
             return i;
     free(block);
     return -1;
