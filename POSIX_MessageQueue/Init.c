32e0b8259aad61ce43640ef301ab8b218c856c44
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-06-29 02:00:07 +0900

    Init() : qcbTblEntry initialization

--- POSIX_MessageQueue/Init.c
+++ POSIX_MessageQueue/Init.c
@@ -2,7 +2,16 @@
 #include "Thread.h"
 #include "Scheduler.h"
 
-void Init(void)
-{
-
+//////////////////////////////////
+/* 이거 헤더 추가하는건지 확인하기 */
+#include "MsgQueue.h"
+//////////////////////////////////
+void Init(void) {
+    for(int i = 0; i < MAX_QCB_NUM; i++){
+        qcbTblEntry[i].name = "null";
+        qcbTblEntry[i].mode = 0;
+        qcbTblEntry[i].openCount = 0;
+        qcbTblEntry[i].pQcb = NULL;
+        qcbTblEntry[i].bUsed = 0;
+    }
 }
