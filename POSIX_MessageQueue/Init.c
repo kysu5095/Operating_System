cdc6fca4f9051e8ed3219918344b4cef7f0361b9
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-06-30 00:53:42 +0900

    add Init.c

--- POSIX_MessageQueue/Init.c
+++ POSIX_MessageQueue/Init.c
@@ -15,4 +15,29 @@ void Init(void) {
         qcbTblEntry[i].pQcb = NULL;
         qcbTblEntry[i].bUsed = 0;
     }
+
+    /* register signal handler */
+	signal(SIGALRM, (void*)RunScheduler);
+	signal(SIGUSR1, (void*)RunScheduler);
+	signal(SIGCHLD, SIG_IGN);
+
+	/* running thread init */
+	pCurrentThread = NULL;
+
+	/* thread table init */
+	for (int i = 0; i < MAX_THREAD_NUM; i++) {
+		pThreadTblEnt[i].bUsed = 0;
+		pThreadTblEnt[i].pThread = NULL;
+	}
+
+	/* ready queue init */
+	for (int i = 0; i < MAX_READYQUEUE_NUM; i++) {
+		pReadyQueueEnt[i].queueCount = 0;
+		pReadyQueueEnt[i].pHead = NULL;
+		pReadyQueueEnt[i].pTail = NULL;
+	}
+
+	/* waiting queue init */
+	pWaitingQueueHead = NULL;
+	pWaitingQueueTail = NULL;
 }
