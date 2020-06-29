9aab6f868fed3b7d443871975ca445c17f25927a
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-06-30 00:46:02 +0900

    Message* deleteQcb() : delete Qcb from msg list head

--- POSIX_MessageQueue/MsgQueue.c
+++ POSIX_MessageQueue/MsgQueue.c
@@ -5,12 +5,10 @@
 void insertQcb(pmqd_t mqd, Message* msg){
     Qcb* qcb = qcbTblEntry[mqd].pQcb;
     qcb->msgCount++;
-    /* head */
-    if(qcb->pMsgHead == NULL){
+    /* msg list is empty */
+    if(qcb->pMsgHead->pNext == qcb->pMsgTail){
         qcb->pMsgHead->pNext = msg;
-        qcb->pMsgHead->pPrev = NULL;
         qcb->pMsgTail->pPrev = msg;
-        qcb->pMsgTail->pNext = NULL;
         msg->pPrev = qcb->pMsgHead;
         msg->pNext = qcb->pMsgTail;
     }
@@ -25,11 +23,30 @@ void insertQcb(pmqd_t mqd, Message* msg){
     }
 }
 
+Message* deleteQcb(pmqd_t mqd){
+    Qcb* qcb = qcbTblEntry[mqd].pQcb;
+    /* msg list is empty */
+    if(qcb->msgCount == 0) return NULL;
+    qcb->msgCount--;
+    Message* msg = qcb->pMsgHead->pNext;
+    qcb->pMsgHead->pNext = msg->pNext;
+    msg->pNext->pPrev = qcb->pMsgHead;
+    msg->pNext = NULL;
+    msg->pPrev = NULL;
+    return msg;
+}
+
+void insertThread(pmqd_t mqd){
+
+}
+
 pmqd_t pmq_open(const char* name, int flags, mode_t perm, pmq_attr* attr) {
     /* check message queue */
     for(int i = 0; i < MAX_QCB_NUM; i++){
-        if(strcmp(qcbTblEntry[i].name, name) == 0)
+        if(strcmp(qcbTblEntry[i].name, name) == 0){
+            qcbTblEntry[i].openCount++;
             return i;
+        }
     }
     /* find empty table row */
     int num = -1;
@@ -46,11 +63,19 @@ pmqd_t pmq_open(const char* name, int flags, mode_t perm, pmq_attr* attr) {
     /* create Qcb */  
     Qcb* qcb = (Qcb*)malloc(sizeof(Qcb));
     qcb->msgCount = 0;
-    qcb->pMsgHead = NULL;
-    qcb->pMsgTail = NULL;
+    qcb->pMsgHead = (Message*)malloc(sizeof(Message));
+    qcb->pMsgTail = (Message*)malloc(sizeof(Message));
+    qcb->pMsgHead->pPrev = NULL;
+    qcb->pMsgHead->pNext = qcb->pMsgTail;
+    qcb->pMsgTail->pPrev = qcb->pMsgHead;
+    qcb->pMsgTail->pNext = NULL;
     qcb->waitThreadCount = 0;
-    qcb->pWaitQHead = NULL;
-    qcb->pWaitQTail = NULL;
+    qcb->pWaitQHead = (Thread*)malloc(sizeof(Thread));
+    qcb->pWaitQTail = (Thread*)malloc(sizeof(Thread));
+    qcb->pWaitQHead->phPrev = NULL;
+    qcb->pWaitQHead->phNext = qcb->pWaitQTail;
+    qcb->pWaitQTail->phPrev = qcb->pWaitQHead;
+    qcb->pWaitQTail->phNext = NULL;
     /* insert Qcb to QcbTblEntry table */
     strcpy(qcbTblEntry[num].name, name);
     qcbTblEntry[num].mode = (int)perm;
@@ -89,6 +114,9 @@ int pmq_send(pmqd_t mqd, char* msg_ptr, size_t msg_len, unsigned int msg_prio) {
     msg->pNext = NULL;
     msg->pPrev = NULL;
     insertQcb(mqd, msg);
+    /////////////////////////
+    /* Waiting Thread 찾기 */
+    /////////////////////////
     return 0;
 }
 
@@ -97,6 +125,14 @@ ssize_t pmq_receive(pmqd_t mqd, char* msg_ptr, size_t msg_len, unsigned int* msg
         perror("pmq_receive : qcbTblEntry is not used");
         return -1;
     }
-    Qcb* qcb = qcbTblEntry[mqd].pQcb;
-    Message* msg = qcb->pMsgHead;
+    Message* msg = deleteQcb(mqd);
+    if(msg == NULL){
+
+    }
+    else{
+        strcpy(msg_ptr, msg->data);
+        msg_prio = msg->priority;
+        free(msg);
+        return msg->size;
+    }
 }
