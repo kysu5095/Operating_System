7f291e76a82b3cc02a85845829b7eeb48b2c75bc
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-07-02 13:21:20 +0900

    add header : string.h, MsgQueue.h

--- POSIX_MessageQueue/Init.c
+++ POSIX_MessageQueue/Init.c
@@ -1,12 +1,9 @@
 #include "Init.h"
 #include "Thread.h"
 #include "Scheduler.h"
+#include "MsgQueue.h"
 #include <string.h>
 
-//////////////////////////////////
-/* 이거 헤더 추가하는건지 확인하기 */
-#include "MsgQueue.h"
-//////////////////////////////////
 void Init(void) {
     for(int i = 0; i < MAX_QCB_NUM; i++){
         strcpy(qcbTblEntry[i].name, "null");
